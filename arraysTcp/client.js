1f4d1ff0a3e18564de629ed1de45d9941b6fd358
Ruslan Gunawardana <rusan.rus@gmail.com>
2016-03-30 08:31:55 +0300

    InterProcessCommunication: Task 1 finished

---
+++ arraysTcp/client.js
@@ -0,0 +1,21 @@
+'use strict';
+const api = {};
+global.api = api;
+api.net = require('net');
+
+const task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+var socket = new api.net.Socket();
+
+socket.connect({
+  port: 8282,
+  host: '127.0.0.1',
+}, () => {
+  socket.write(JSON.stringify(task));
+  socket.on('data', data => {
+    let result = JSON.parse(data);
+    console.log(result);
+    socket.destroy(); // kill client after server's response
+  });
+});
+
+
